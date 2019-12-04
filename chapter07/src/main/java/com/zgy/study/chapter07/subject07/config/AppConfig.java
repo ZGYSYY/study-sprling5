9f748e4950a4953597ecbc97058b849ef3db6e88
ZGY <3030392760@qq.com>
2019-12-04 11:41:22 +0800

    完善代码

---
+++ chapter07/src/main/java/com/zgy/study/chapter07/subject07/config/AppConfig.java
@@ -0,0 +1,70 @@
+package com.zgy.study.chapter07.subject07.config;
+
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author ZGY <br>
+ * @date 2019/12/3 11:05 <br>
+ * @description AppConfig <br>
+ */
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.zgy.study.chapter07.subject07.dao.impl")
+public class AppConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppConfig.class);
+
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.H2)
+                .addScripts("classpath:subject07/db/h2/schema.sql")
+                .addScripts("classpath:subject07/db/h2/test-data.sql")
+                .build();
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        properties.put("hibernate.format_sql", true);
+        properties.put("hibernate.use_sql_comments", true);
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.max_fetch_depth", 3);
+        properties.put("hibernate.jdbc.batch_size", 10);
+        properties.put("hibernate. dbc.fetch_size", 50);
+        // 设置 Hibernate 的一些基本属性
+        sessionFactoryBean.setHibernateProperties(properties);
+        // 设置实体类的包位置
+        sessionFactoryBean.setPackagesToScan("com.zgy.study.chapter07.subject07.entities");
+        try {
+            sessionFactoryBean.afterPropertiesSet();
+        } catch (IOException e) {
+            LOGGER.error("=============> sessionFactoryBean.afterPropertiesSet() 发生错误", e);
+            return null;
+        }
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new HibernateTransactionManager(sessionFactory());
+    }
+}
---
+++ chapter07/src/test/java/com/zgy/study/chapter07/subject07/AppTest.java
@@ -0,0 +1,185 @@
+package com.zgy.study.chapter07.subject07;
+
+import com.zgy.study.chapter07.subject07.config.AppConfig;
+import com.zgy.study.chapter07.subject07.dao.SingerDao;
+import com.zgy.study.chapter07.subject07.entities.Album;
+import com.zgy.study.chapter07.subject07.entities.Instrument;
+import com.zgy.study.chapter07.subject07.entities.Singer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author ZGY <br/>
+ * @date 2019/12/3 15:18 <br/>
+ * @description AppTest <br/>
+ */
+public class AppTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppTest.class);
+
+    private AnnotationConfigApplicationContext context = null;
+
+    @Before
+    public void before() {
+        context = new AnnotationConfigApplicationContext(AppConfig.class);
+    }
+
+    @Test
+    public void test1() {
+        LOGGER.info("==================> 执行 public void test1() 方法 =========> GO");
+        SingerDao singerDao = context.getBean("singerDaoImpl", SingerDao.class);
+        List<Singer> singerList = singerDao.findAll();
+        for (Singer singer : singerList) {
+            LOGGER.info("singer: [{}]", singer);
+            if (!singer.getAlbums().isEmpty()) {
+                for (Album album : singer.getAlbums()) {
+                    LOGGER.info("== album: [{}]", album);
+                }
+            }
+            if (!singer.getInstruments().isEmpty()) {
+                for (Instrument instrument : singer.getInstruments()) {
+                    LOGGER.info("== instrument: [{}]", instrument);
+                }
+            }
+        }
+        LOGGER.info("==================> 执行 public void test1() 方法 =========> END");
+    }
+
+    @Test
+    public void test2() {
+        SingerDao singerDao = context.getBean("singerDaoImpl", SingerDao.class);
+        List<Singer> singerList = singerDao.findAllWithAlbum();
+        if (singerList != null) {
+            LOGGER.info("===================> 打印数据");
+            for (Singer singer : singerList) {
+                LOGGER.info("singer: [{}]", singer);
+                if (!singer.getAlbums().isEmpty()) {
+                    for (Album album : singer.getAlbums()) {
+                        LOGGER.info("== album: [{}]", album);
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    public void test3() {
+        SingerDao singerDao = context.getBean("singerDaoImpl", SingerDao.class);
+        Singer singer = singerDao.findById(1L);
+        LOGGER.info("singer: [{}]", singer);
+        if (!singer.getAlbums().isEmpty()) {
+            for (Album album : singer.getAlbums()) {
+                LOGGER.info("== album: [{}]", album);
+            }
+        }
+        if (!singer.getInstruments().isEmpty()) {
+            for (Instrument instrument : singer.getInstruments()) {
+                LOGGER.info("== instrument: [{}]", instrument);
+            }
+        }
+    }
+
+    @Test
+    public void test4() throws ParseException {
+        SingerDao singerDao = context.getBean("singerDaoImpl", SingerDao.class);
+
+        Singer singer = new Singer();
+        singer.setFirstName("Z");
+        singer.setLastName("GY");
+        singer.setBirthDate(new SimpleDateFormat("yyyy-MM-dd").parse("1996-11-18"));
+
+        Set<Album> albums = new LinkedHashSet<>(2);
+        Album album = new Album();
+        album.setTitle("AAAAAAAAAAAAAA");
+        album.setReleaseDate(new SimpleDateFormat("yyyy-MM-dd").parse("1996-11-19"));
+        album.setSinger(singer);
+        albums.add(album);
+        album = new Album();
+        album.setTitle("BBBBBBBBBBBBBB");
+        album.setReleaseDate(new SimpleDateFormat("yyyy-MM-dd").parse("1996-11-20"));
+        album.setSinger(singer);
+        albums.add(album);
+        singer.setAlbums(albums);
+
+        Set<Instrument> instruments = new LinkedHashSet<>(1);
+        Instrument instrument = new Instrument();
+        instrument.setInstrumentId("Piano");
+        instruments.add(instrument);
+        singer.setInstruments(instruments);
+        singer = singerDao.save(singer);
+
+        LOGGER.info("singer: [{}]", singer);
+        if (singer.getId() != null) {
+            singer = singerDao.findById(singer.getId());
+            LOGGER.info("singer: [{}]", singer);
+            if (!singer.getAlbums().isEmpty()) {
+                for (Album singerAlbum : singer.getAlbums()) {
+                    LOGGER.info("==> singerAlbum: [{}]", singerAlbum);
+                }
+            }
+            if (!singer.getInstruments().isEmpty()) {
+                for (Instrument singerInstrument : singer.getInstruments()) {
+                    LOGGER.info("==> singerInstrument: [{}]", singerInstrument);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void test5() {
+        SingerDao singerDao = context.getBean("singerDaoImpl", SingerDao.class);
+        Singer singer = singerDao.findById(1L);
+        Assert.assertNotNull(singer);
+        Assert.assertEquals("John", singer.getFirstName());
+
+        Album album = singer.getAlbums().stream().filter(a -> a.getTitle().equals("Battle Studies")).findFirst().get();
+
+        singer.setFirstName("Z");
+        singer.getAlbums().remove(album);
+
+        singerDao.save(singer);
+
+        LOGGER.info("singer: [{}]", singer);
+        if (singer.getId() != null) {
+            singer = singerDao.findById(singer.getId());
+            LOGGER.info("singer: [{}]", singer);
+            if (!singer.getAlbums().isEmpty()) {
+                for (Album singerAlbum : singer.getAlbums()) {
+                    LOGGER.info("==> singerAlbum: [{}]", singerAlbum);
+                }
+            }
+            if (!singer.getInstruments().isEmpty()) {
+                for (Instrument singerInstrument : singer.getInstruments()) {
+                    LOGGER.info("==> singerInstrument: [{}]", singerInstrument);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void test6() {
+        SingerDao singerDao = context.getBean("singerDaoImpl", SingerDao.class);
+        Singer singer = singerDao.findById(2L);
+        Assert.assertNotNull(singer);
+        singerDao.delete(singer);
+        test2();
+    }
+
+    @After
+    public void after() {
+        if (context != null) {
+            context.close();
+        }
+    }
+}
